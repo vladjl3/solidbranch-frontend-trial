0e563ab06cd1d971b1c99d805e538b60439f740e
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-11-09 18:23:44 +0600

    feat: add lazy-loading home page module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
+    path: "",
+    loadChildren: () => import("./pages/home-page/home-page.module").then((m) => m.HomePageModule),
+  },
+  {
     path: "navigator",
     loadChildren: () => import("./pages/navigator/navigator.module").then((m) => m.NavigatorModule),
   },
