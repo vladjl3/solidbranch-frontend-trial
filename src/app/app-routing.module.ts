8efe6a1c83957edf7162f0819e6904c73af00777
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-11-09 17:30:52 +0600

    feat: add navigator lazy-loading page module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "navigator",
+    loadChildren: () => import("./pages/navigator/navigator.module").then((m) => m.NavigatorModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/navigator/container/navigator.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "sft-navigator",
+  templateUrl: "./navigator.component.html",
+  styleUrls: ["./navigator.component.scss"],
+})
+export class NavigatorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
